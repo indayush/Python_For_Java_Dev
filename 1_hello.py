7ba10da2e164cb1aa9373d1e118e6f3840578f1f
Ayush Adarsh <ayushadarsh03@gmail.com>
2020-06-30 13:01:28 +0530

    Comments added about quotes used in functions

--- 1_hello.py
+++ 1_hello.py
@@ -7,5 +7,6 @@ print ('Hello')
 # My Observations -                      - # is for single line comment
 '''                                      - this is for multi line comment. It is like /* ....*/ in java
 Capital P not allowed for print
+Both Single & double quotes supported in functions for strings
 
 '''
